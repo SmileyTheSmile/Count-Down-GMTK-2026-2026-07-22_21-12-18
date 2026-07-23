5b5efdf1d7f49e039cc6bdc326c89c2695ef7c12
SmileyTheSmile <34063023+SmileyTheSmile@users.noreply.github.com>
2026-07-23 19:00:34 +0300

    Updated player movement

--- Assets/Entities/Player/Player.cs
+++ Assets/Entities/Player/Player.cs
@@ -2,43 +2,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private CollisionCheck checkDown;
-    [SerializeField] private CollisionCheck checkUp;
-    [SerializeField] private CollisionCheck checkLeft;
-    [SerializeField] private CollisionCheck checkRight;
+    [SerializeField] private CollisionCheck _checkDown;
+    [SerializeField] private CollisionCheck _checkUp;
+    [SerializeField] private CollisionCheck _checkLeft;
+    [SerializeField] private CollisionCheck _checkRight;
 
-    [SerializeField] private Rigidbody2D rigidbody;
+    [SerializeField] private Rigidbody2D _rigidbody;
 
-    [SerializeField] private Vector2 gravity = new Vector2(0.00f, -9.81f);
-    [SerializeField] private Vector2 gravityDirection = new Vector2(0.00f, -1f);
+    [SerializeField] private float _acceleration = 10f;
+    [SerializeField] private float _maxSpeed = 10f;
+
+    private Vector2 _gravityDirection = Vector2.down;
+    private float _currSpeed = 0;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            gravity = new Vector2(0.00f, 9.81f);
-            rigidbody.velocity = new Vector2(0f, rigidbody.velocity.y);
+            _gravityDirection = Vector2.up;
+            _rigidbody.velocity = new Vector2(0f, _rigidbody.velocity.y);
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            gravity = new Vector2(0.00f, -9.81f);
-            rigidbody.velocity = new Vector2(0f, rigidbody.velocity.y);
+            _gravityDirection = Vector2.down;
+            _rigidbody.velocity = new Vector2(0f, _rigidbody.velocity.y);
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
-            gravity = new Vector2(-9.81f, 0.00f);
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0f);
+            _gravityDirection = Vector2.left;
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.y, 0f);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            gravity = new Vector2(9.81f, 0.00f);
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0f);
+            _gravityDirection = Vector2.right;
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.y, 0f);
         }
 
-        rigidbody.velocity += gravity * Time.deltaTime;
+        _currSpeed = Mathf.MoveTowards(_currSpeed, _maxSpeed, _acceleration * Time.fixedDeltaTime);
+        _rigidbody.velocity = _currSpeed * _gravityDirection;
     }
 }
