1d21abbabec3ed8c44337137a29e3752a73ba8bc
SmileyTheSmile <34063023+SmileyTheSmile@users.noreply.github.com>
2026-07-26 20:46:34 +0300

    Fixed chains

--- Assets/Chain.cs
+++ Assets/Chain.cs
@@ -4,4 +4,12 @@ using UnityEngine;
 
 public class Chain : MonoBehaviour
 {
+    [SerializeField] BoxCollider2D _collider;
+    [SerializeField] private float reloadInterval = 1.0f;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<Player>().CanHook = true;
+    }
+
 }
